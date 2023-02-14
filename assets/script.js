c6ef552a35287fa8f26a651e9a608c8938e752d9
Devin Polichetti <devinpolichetti@gmail.com>
2023-02-14 16:10:38 -0500

    added dependencies in script.js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//Dependencies
+var startButton = document.getElementById("start");
+var timerEl = document.getElementById("timer-indicator");
+var timeLeftEl = document.getElementById("time-left");
+var questionEl = document.getElementById("question-display");
+var ruleEl = document.getElementById("rules");
+var answerListEl = document.getElementById("answer-list");
+var answerChoice1 = document.getElementById("answer-1");
+var answerChoice1 = document.getElementById("answer-2");
+var answerChoice1 = document.getElementById("answer-3");
+var answerChoice1 = document.getElementById("answer-4");
+var questionResults = document.getElementById("question-results");
